13af926fa8fa94a924fbdf5a8cae86a156f77439
Zomarot <dbhschool127996@gmail.com>
2024-07-10 16:54:20 +0600

    a

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -3,6 +3,12 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Search, TrendingUp } from "lucide-react";
 import React from "react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 const page = () => {
   return (
@@ -15,7 +21,7 @@ const page = () => {
         </Button>
       </div>
       {/* product */}
-      <section className="p-4 ring-1 ring-border mt-4 rounded-md bg-slate-100/50">
+      <section className="p-4 ring-1 ring-border mt-4 rounded-md bg-slate-100/20">
         {/* section header  */}
         <div className="flex items-center justify-between py-4 pb-8 ">
           <div className="flex items-center gap-4">
@@ -27,8 +33,27 @@ const page = () => {
         {/* proudcts  */}
         <div className="grid grid-cols-5 gap-4">
           <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
         </div>
       </section>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger>Hover</TooltipTrigger>
+          <TooltipContent>
+            <p>Add to library</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
     </div>
   );
 };
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -6,25 +6,29 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Heart, Search, ShoppingBag } from "lucide-react";
+import { Eye, Heart, Search, ShoppingBag } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
 const ProductCard = () => {
   return (
     <Card className="group relative">
-      <div className="flex justify-between items-center mb-2 p-2">
-        <span className="px-2 py-1 rounded-full text-xs bg-green-500 text-white">
-          new
-        </span>
+      <div className="flex justify-between items-center p-2">
         <span className="px-2 py-1 rounded-full text-xs bg-orange-500 text-white">
           -50%
         </span>
+        {/* <span className="px-2 py-1 rounded-full text-xs bg-green-500 text-white">
+          best selling
+        </span> */}
       </div>
-      <div className="">
-        <div className="relative w-full min-h-[200px] ">
-          <Image src={"/img/1.webp"} fill alt="" className="object-cover" />
-        </div>
+      <div className=" overflow-hidden">
+        <Image
+          src={"/img/1.webp"}
+          width={180}
+          height={180}
+          alt={"title"}
+          className="aspect-square h-fit w-full hover:scale-110 transition-all"
+        />
       </div>
       <div className="p-4 flex items-center justify-center flex-col gap-3">
         <Link
@@ -46,7 +50,7 @@ const ProductCard = () => {
           <ShoppingBag size={16} />
         </span>
         <span className="h-8 w-8 rounded-full bg-zinc-800 flex items-center justify-center text-white hover:bg-lime-500 transition-all duration-300 cursor-pointer">
-          <Search size={16} />
+          <Eye size={16} />
         </span>
       </div>
     </Card>
